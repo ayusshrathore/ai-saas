b40728feb7c3820f6a7b1e6f349fe8798f92e814
ayusshrathore <ayusshrathore@gmail.com>
2023-07-23 16:24:12 +0530

    feat: add video generation page

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Video } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Empty } from "@/components/empty";
+import { Heading } from "@/components/heading";
+import { Loader } from "@/components/loader";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { formSchema } from "./constants";
+
+const VideoPage = () => {
+	const router = useRouter();
+	const [video, setVideo] = useState("");
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			prompt: "",
+		},
+	});
+
+	const isLoading = form.formState.isSubmitting;
+
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		console.log(values);
+		try {
+			setVideo("");
+			const response = await axios.post("/api/music");
+			setVideo(response.data.audio);
+
+			form.reset();
+		} catch (error) {
+			// TODO: Open Pro Modal
+			console.log(error);
+		} finally {
+			router.refresh();
+		}
+	};
+
+	return (
+		<div>
+			<Heading
+				title="Video Generation"
+				description="Our most advanced AI Video Generation model."
+				icon={Video}
+				iconColor="text-orange-700"
+				bgColor="bg-orange-700/10"
+			/>
+			<div className="px-4 lg:px-8">
+				<div>
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(onSubmit)}
+							className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+						>
+							<FormField
+								name="prompt"
+								render={({ field }) => (
+									<FormItem className="col-span-12 lg:col-span-10">
+										<FormControl className="m-0 p-0">
+											<Input
+												{...field}
+												placeholder="Start typing here..."
+												className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+												disabled={isLoading}
+											/>
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<Button
+								className="col-span-12 lg:col-span-2 w-full"
+								disabled={isLoading}
+							>
+								Generate
+							</Button>
+						</form>
+					</Form>
+				</div>
+				<div className="space-y-4 mt-4">
+					{isLoading && (
+						<div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+							<Loader />
+						</div>
+					)}
+					{!video && !isLoading && (
+						<Empty label="Start typing to generate videos." />
+					)}
+					{video && (
+						<video
+							className="w-full aspect-video mt-8 rounded-lg border bg-black"
+							controls
+						>
+							<source src={video} />
+						</video>
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default VideoPage;
---
+++ app/api/video/route.ts
@@ -0,0 +1,37 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+	auth: process.env.REPLICATE_API_TOKEN!,
+});
+
+export async function POST(req: Request) {
+	try {
+		const { userId } = auth();
+		const body = await req.json();
+		const { prompt } = body;
+
+		if (!userId) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+
+		if (!prompt) {
+			return new NextResponse("Prompt is required", { status: 400 });
+		}
+
+		const response = await replicate.run(
+			"anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+			{
+				input: {
+					prompt,
+				},
+			}
+		);
+
+		return NextResponse.json(response, { status: 200 });
+	} catch (error) {
+		console.log("[VIDEO_ERROR]", error);
+		return new NextResponse("Internal Server Error", { status: 500 });
+	}
+}

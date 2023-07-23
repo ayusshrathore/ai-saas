6d77bb53298b373f8caed6d61c7579bb7e6add0b
ayusshrathore <ayusshrathore@gmail.com>
2023-07-23 15:54:00 +0530

    chore: add image domains in next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		domains: ["oaidalleapiprodscus.blob.core.windows.net"],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

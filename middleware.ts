dd59192ae25f021c0501c79b92909ec879f6bcbc
ayusshrathore <ayusshrathore@gmail.com>
2023-07-23 13:17:45 +0530

    chore: add middleware for clerk auth

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
+export default authMiddleware({
+  publicRoutes: ["/"],
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};

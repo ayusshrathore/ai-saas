d29757f9683f49344dfa293bea6f10bbdd6af62d
ayusshrathore <ayusshrathore@gmail.com>
2023-07-30 11:51:33 +0530

    fix: stripe webhook giving 401 error

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/webhook"],
 });
 
 export const config = {
